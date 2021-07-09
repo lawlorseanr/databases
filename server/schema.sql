54f05c134444eff5fdda3b72e5145196e2ec97b7
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-08 17:18:10 -0700

    Implement basic database schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,30 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  github VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
-/* Create other tables and define schemas for them here! */
-
-
+CREATE TABLE messages (
+  id INT AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  campus VARCHAR(255) NOT NULL,
+  roomname VARCHAR(255) NOT NULL,
+  content VARCHAR(255) NOT NULL,
+  created_at DATE NOT NULL,
+  updated_at DATE NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
 
 
 /*  Execute this file from the command line by typing:
